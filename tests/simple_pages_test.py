8f73225fa009af33e33d18647a251b7283bfd9d3
AshishSidana <ashish.sidana@gmail.com>
2022-05-06 06:07:10 -0400

    Write unit test for when request url not found

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -17,13 +17,18 @@ def test_request_index(client):
     assert b"Index" in response.data
 
 def test_request_about(client):
-    """This makes the index page"""
+    """This makes the about page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About" in response.data
 
  def test_request_welcome(client):
-    """This makes the index page"""
+    """This makes the welcome page"""
     response = client.get("/welcome")
     assert response.status_code == 200
     assert b"welcome" in response.data
+
+def test_request_page_not_found(client):
+    """Test page not found"""
+    response = client.get("/page5")
+    assert response.status_code == 404
