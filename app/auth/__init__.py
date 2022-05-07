c6d59fbf01e0307956ff1b5531439d2bfcceea08
AshishSidana <ashish.sidana@gmail.com>
2022-05-07 13:25:59 -0400

    Create login feature & write unit test cases

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -30,3 +30,31 @@ def register():
             return redirect(url_for('auth.login'), 302)
     return render_template('register.html', form=form)
 
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    form = login_form()
+    if current_user.is_authenticated:
+        return redirect(url_for('auth.dashboard'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('auth.login'))
+        else:
+            user.authenticated = True
+            db.session.add(user)
+            db.session.commit()
+            login_user(user)
+            flash("Welcome", 'success')
+            return redirect(url_for('auth.dashboard'))
+    return render_template('login.html', form=form)
+
+
+@auth.route('/dashboard', methods=['GET'], defaults={"page": 1})
+@auth.route('/dashboard/<int:page>', methods=['GET'])
+@login_required
+def dashboard(page):
+    return render_template('dashboard.html')
+
+
+
