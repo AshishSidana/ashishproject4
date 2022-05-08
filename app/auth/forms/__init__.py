064f4f70107f8bec16a0fbb6a8de06886309ee8f
AshishSidana <ashish.sidana@gmail.com>
2022-05-08 02:57:29 -0400

    Manage account featurfe & unit test cases

--- app/auth/forms/__init__.py
+++ app/auth/forms/__init__.py
@@ -33,3 +33,18 @@ class profile_form(FlaskForm):
                           description="Please add information about yourself")
 
     submit = SubmitField()
+
+class security_form(FlaskForm):
+    email = EmailField('Email Address', [
+        validators.DataRequired(),
+
+    ], description="You can change your email address")
+
+    password = PasswordField('Create Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+
+    ], description="Create a password ")
+    confirm = PasswordField('Repeat Password', description="Please retype your password to confirm it is correct")
+
+    submit = SubmitField()
