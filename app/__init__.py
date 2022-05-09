6dd39fd83b962809b5fa2c58ae3f94b411cdd24b
AshishSidana <ashish.sidana@gmail.com>
2022-05-09 00:52:32 -0400

    Upload account transaction CSV & unit test cases

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,7 @@ from app.db.models import User
 from app.error_handlers import error_handlers
 from app.logging_config import log_con, LOGGING_CONFIG
 from app.simple_pages import simple_pages
+from app.bank_account import bank_account
 from app.db import database
 from flask_cors import CORS
 login_manager = flask_login.LoginManager()
@@ -43,6 +44,7 @@ def create_app():
     # these load functions with web interface
     app.register_blueprint(simple_pages)
     app.register_blueprint(auth)
+    app.register_blueprint(bank_account)
     app.register_blueprint(database)
     # these load functionality without a web interface
     app.register_blueprint(log_con)
